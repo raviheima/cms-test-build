25407c8a7e33b4c9280e51948e66fea4643740c8
Aankhen <Aankhen@users.noreply.github.com>
2023-08-12 22:00:04 +0530

    refactor: move .eleventy.js to top level

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function(eleventyConfig) {
     
-    eleventyConfig.addPassthroughCopy('./src/assets');
+    eleventyConfig.addPassthroughCopy('src/assets');
 
     return{
         dir:{
