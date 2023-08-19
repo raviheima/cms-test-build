71dc7c53f76eac4a0b3a31e80ba8bacdaca0f818
Victor Agu <vctroreo@gmail.com>
2023-08-19 22:57:44 +0100

    complete build
    complete templating for eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,29 @@
 const { DateTime } = require("luxon");
- 
+
 module.exports = function(eleventyConfig) {
     
     eleventyConfig.addPassthroughCopy('./src/assets');
 
-    eleventyConfig.addFilter("postDate", (dateObj) =>{
-      return DateTime.fromJSDate(dateObj).toLocaleString(DateTime)
-}) 
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime);
+    });
 
-    return{
-        dir:{
+    eleventyConfig.setServerOptions({
+        liveReload: true,
+        domDiff: true,
+        port: 8080,
+        watch: [],
+        showAllHosts: true,
+        https: {
+            // key: "./localhost.key",
+            // cert: "./localhost.cert",
+        },
+        encoding: "utf-8",
+        showVersion: false,
+    });
+
+    return {
+        dir: {
             input: "src",
             output: "public"
         }
